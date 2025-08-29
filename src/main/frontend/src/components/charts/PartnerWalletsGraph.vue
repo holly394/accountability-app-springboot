2adb2e61fc65c21ecf05a66506613d39f6085c86
holly394 <hoyeonyoo@gmail.com>
2025-08-29 17:17:37 +0200

    changed path in frontend for one import in PartnerWalletsGraph.vue

--- src/main/frontend/src/components/charts/PartnerWalletsGraph.vue
+++ src/main/frontend/src/components/charts/PartnerWalletsGraph.vue
@@ -88,7 +88,7 @@ onUnmounted(() => {
 </script>
 
 <style lang="scss" scoped>
-@import '/home/holly/IdeaProjects/accountability-app-springboot/src/main/frontend/src/css/quasar.variables.scss';
+@import 'src/css/quasar.variables.scss';
 
 .rounded-outer-card {
   @include card-style;
